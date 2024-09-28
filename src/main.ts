d9d574c0e0a7320d8d8addfbc5be26f94b2f76f7
LucasMRC <lucas.reynacordoba22@gmail.com>
2024-09-28 09:17:04 +0200

    fixes error log

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import './reset.css'
 import App from './App.svelte';
 
 const app = new App({
-  target: document.getElementById('app'),
+  target: document.getElementById('app') as HTMLElement,
 })
 
 export default app
